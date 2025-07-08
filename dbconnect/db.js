f805da88ef8b36f49280b17e892cb53487e4d2d9
kamrul111 <si2271485@gmail.com>
2025-07-08 08:55:48 +0600

    added

--- dbconnect/db.js
+++ dbconnect/db.js
@@ -1,36 +1,5 @@
 
 
-// adeptshaharia
-// C9DWzWZzkBwSpU80
-// const mongoose = require("mongoose");
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect("mongodb://127.0.0.1:27017/testProductDb");
-//     console.log("MongoDB connected successfully");
-
-//     const collections = await mongoose.connection.db.listCollections().toArray();
-//     const hasProductsCollection = collections.some(col => col.name === 'products');
-
-//     if (hasProductsCollection) {
-//       const indexes = await mongoose.connection.collection('products').indexes();
-//       const hasNameIndex = indexes.find(index => index.name === 'name_1');
-
-//       if (hasNameIndex) {
-//         await mongoose.connection.collection('products').dropIndex('name_1');
-//         console.log("Dropped index: name_1");
-//       } else {
-//         console.log("No index named 'name_1' found.");
-//       }
-//     } else {
-//       console.log("Collection 'products' does not exist.");
-//     }
-
-//   } catch (err) {
-//     console.error("MongoDB connection failed:", err.message);
-//     process.exit(1);
-//   }
-// };
-
 // module.exports = connectDB;
 const mongoose = require("mongoose");
 
@@ -44,8 +13,8 @@ const connectDB = async () => {
     await mongoose.connect(uri, {
       // Mongoose 6.x and later automatically handle these options,
       // but they are kept for clarity and compatibility with older versions.
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
       serverSelectionTimeoutMS: 30000, // Keep trying to send operations for 30 seconds
       socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
     });

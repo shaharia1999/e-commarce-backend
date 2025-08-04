ce07bfa40e18f58ec92417bf36017f5013475c86
shaharia <adeptshaharia@gmail.com>
2025-08-05 01:10:39 +0600

    push

--- dbconnect/db.js
+++ dbconnect/db.js
@@ -1,6 +1,6 @@
 
 const mongoose = require("mongoose");
-const uri = process.env.MONGO_URI;
+const uri = process.env.MONGODB_URI ;
 const connectDB = async () => {
   try {
     await mongoose.connect(uri, {

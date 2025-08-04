c2c97a285ac03e3998c1fd7429b4d9034428b9d2
shaharia <adeptshaharia@gmail.com>
2025-08-05 01:13:51 +0600

    push

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -6,18 +6,17 @@ const verifyToken = (req, res, next) => {
   if (!authHeader?.startsWith('Bearer ')) {
     return res.status(401).send({ message: 'Unauthorized' });
   }
-
   const token = authHeader.split(' ')[1];
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log("✅ Decoded user:", decoded); // ✅ Add this line
+    console.log(" Decoded user:", decoded);
+  
     req.user = decoded;
     next();
   } catch (err) {
     res.status(403).send({ message: 'Invalid or expired token' });
   }
 };
-
 module.exports = verifyToken;
 
